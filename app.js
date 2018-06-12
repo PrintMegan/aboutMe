95c04096c902b75fa12be30ee6d364a7bf2df5aa
MEGAN IVEY <PRINTMEGANIVEY@GMAIL.COM>
2018-06-12 12:35:55 -0700

    Added new JS

---
+++ app.js
@@ -0,0 +1,38 @@
+'use strict'; 
+
+//as a developer, I want to ask interactive questions, so that the program is personalized
+
+alert('Am I a princess?');
+
+var userName = prompt('Welcome, what is your name?');
+alert('Hi ' + userName.)
+
+var wings = prompt('Do you think I have wings?');
+if(wings == 'Yes'){
+  alert('I do NOT have wings.');
+else('You\'re right, I DON\'T have wings! ')
+}
+
+var crown = prompt('Do you think I have a crown?')
+if(crown == 'yes'){
+  alert('You\'re right! I do wear a crown!');
+  else ('Wrong! I DO wear a crown!');
+}
+
+
+var castle = prompt('Do you think I live in a castle?');
+if(castle == 'yes'){
+  alert('You\'re right! I DO live in a castle!');
+else ('Wrong, I DO live in a castle!');
+}
+
+var princess = prompt('Do you think i\'m a princess?');
+if(princess == 'yes'){
+  alert('You\'re right! I AM a princess. And so are you!')
+else('Sorry! I AM a princess!');
+
+}
+
+
+
+
