47227acb6f7b1647abfbc2c242ef7012af300851
MEGAN IVEY <PRINTMEGANIVEY@GMAIL.COM>
2018-06-13 15:21:09 -0700

    added new js

---
+++ app.js
@@ -0,0 +1,39 @@
+'use strict';
+
+// alert('Welcome to my site, let\'s play a guessing game about me. ');
+
+// alert('these are yes and no questions,you must answer with either (Y) or (N)');
+
+// var username = prompt('what is your name? ');
+
+// var username = 'megan';
+
+// while(true){
+
+// var WereYouBorn = prompt('was I born?').toLowerCase();
+
+// if(wereYouBorn === 'y' || wereYouBorn === 'yes'){
+//   alert('of course I was');
+// } else if(wereYouborn === 'n' || wereYouBorn === 'no' ){
+//   alert('how did you find out my secret? ');
+// } else {
+//   alert('please answer yes or no');
+//  }
+
+//  var favoriteFoods = ['yogurt', 'bananas'];
+//  for(var incrementor = 0; incrementor < favoriteFoods.length; incrementor++){
+// console.log(favoriteFoods[incrementor]);
+//  }
+
+//counter
+var counterForFoods = 0;
+
+while(counterForFoods < 3){
+  var foodGuess = prompt('what is one of my favorite foods? ');
+
+  if(foodGuess === 'pizza'){
+    alert('nice job');
+    break;
+    counterForFoods++;
+  }
+}
--- js/aboutMegan.js
+++ js/aboutMegan.js
@@ -1,64 +1,97 @@
-'use strict'; 
+'use strict';
 
 //as a developer, I want to ask interactive questions, so that the program is personalized
 
-alert('A little about Megan');
+// alert('Welcome to my site, let\'s play a game');
+// alert('these are yes and no questions,you must answer with either (Y) or (N)');
+// var userName = prompt('What is your name?');
+// alert('Hi ' + userName);
+// console.log('What is your name? ' + userName);
 
-var userName = prompt('Welcome, what is your name?');
-alert('Hi ' + userName);
-console.log('What is your name? ' + userName); 
+// //Do you know me?
+// var doYouKnowMe = prompt('Do you know anything about me already?');
+// doYouKnowMe = doYouKnowMe.toLowerCase();
 
-//Do you know me?
-var doYouKnowMe = prompt('Do you know anything about me already?');
-doYouKnowMe = doYouKnowMe.toLowerCase();
+// if (doYouKnowMe === 'yes' || doYouKnowMe === 'y') {
+//   alert('So now you\'re a Megan expert?');
+// } else if (doYouKnowMe === 'no' || doYouKnowMe === 'n') {
+//   alert('How have you not already heard about me? ');
+// } else {
+//   alert('Please respond yes or no');
+// }
+// console.log(' Do you know about me ' + doYouKnowMe);
 
-if (doYouKnowMe === 'yes' || doYouKnowMe === 'y'){
-  alert('So now you\'re a Megan expert?');
-} else if ( doYouKnowMe === 'no' || doYouKnowMe === 'n'){
-  alert('How have you not already heard about me? ');
-} else { 
-  alert('Please respond yes or no');
+// //Am I a dog person?
+// var dogPerson = prompt('Do you think I\'m a dog person?');
+// dogPerson = dogPerson.toLowerCase();
+
+// if (dogPerson === 'yes' || dogPerson === 'y') {
+//   alert('I can\'t stand slobbery dogs!');
+// } else if (dogPerson === 'no' || dogPerson === 'n') {
+//   alert('You\'re right, cats are where it\'s at!');
+// } else {
+//   alert('Please respond yes or no');
+// }
+// console.log('Do you think I\'m a dog person' + dogPerson);
+
+// //Do you think I can hop on one foot while rubbing my belly 
+// var hop = prompt('Do you think I can hop on one foot while rubbing my belly?');
+// hop = hop.toLowerCase();
+
+// if (hop === 'yes' || hop === 'y') {
+//   alert('Heck yes I can!!');
+// } else if (hop === 'no' || chop === 'n') {
+//   alert('Do you think I\'m uncoordinated?');
+// } else {
+//   alert('Please respond yes or no');
+// }
+// console.log('Do you think I can hop on one foot while rubbing my belly? ' + hop);
+
+// //Do you think I'm a bad ass already
+// var badass = prompt('Do you think i\'m a badass yet?');
+// badass = badass.toLowerCase();
+
+// if (badass === 'yes' || badass === 'y') {
+//   alert('Heck fricking yea I\'m a badass!');
+// } else if (badass === 'no' || badass === 'n') {
+//   alert('WOW. You underestimate me Sir!');
+// } else {
+//   alert('Please respond with yes or no');
+// }
+// console.log('Do you think I am a badass ' + badass);
+
+
+//extra 2 questions
+
+//How many kids do you think I have?
+
+alert('let\'s play a guessing game. You\'ll have 4 tries');
+var howManyKids = prompt('How many kids do I have? ');
+var kidsCounter;
+
+for (kidsCounter = 0; kidsCounter < 3; kidsCounter++) {
+  console.log(kidsCounter);
+
+  if (howManyKids > 2) {
+
+    console.log('greater than 2');
+    howManyKids = prompt('Sorry, too high. Try again');
+  }
+  else if (howManyKids < 2) {
+    console.log('less than 2');
+
+    howManyKids = prompt('Sorry, too low. Try again');
+  }
+  else if (howManyKids == 2) {
+    console.log('equal to 2');
+    alert('You\'re right!!');
+    break;
+  }
+  if ( kidsCounter === 2 ){
+    alert('You\'re all out of tries');
+  }
 }
-console.log(' Do you know about me ' + doYouKnowMe);
 
-//Am I a dog person?
-var dogPerson = prompt('Do you think I\'m a dog person?');
-dogPerson = dogPerson.toLowerCase();
-
-if (dogPerson === 'yes' || dogPerson === 'y' ){
-  alert('I can\'t stand slobbery dogs!' );
-} else if (dogPerson === 'no' || dogPerson === 'n'){
-  alert('You\'re right, cats are where it\'s at!');
-} else { 
-  alert('Please respond yes or no');
-}
-console.log('Do you think I\'m a dog person' + dogPerson);
-
-//Do you think I can hop on one foot while rubbing my belly 
-var hop = prompt('Do you think I can hop on one foot while rubbing my belly?');
-hop = hop.toLowerCase();
-
-if (hop === 'yes' || hop === 'y'){
-  alert('Heck yes I can!!');
-} else if (hop === 'no' || chop === 'n') {
-  alert('Do you think I\'m uncoordinated?');
-} else {
-  alert('Please respond yes or no');
-}
-console.log('Do you think I can hop on one foot while rubbing my belly? ' + hop);
-
-//Do you think I'm a bad ass already
-var badass = prompt('Do you think i\'m a badass yet?');
-badass = badass.toLowerCase();
-
-if(badass === 'yes' || badass === 'y'){
-  alert ('Heck fricking yea I\'m a badass!');
-} else if (badass === 'no' || badass === 'n'){
-  alert('WOW. You underestimate me Sir!' );
-} else {
-  alert('Please respond with yes or no');
-}
-console.log('Do you think I am a badass ' + badass);
 
 
 
